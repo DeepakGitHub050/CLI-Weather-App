244b8ba51f3e10378f56d0006b1bebad4c9e4589
DeepakGitHub050 <ds1008076@gmail.com>
2022-06-30 15:20:48 +0530

    Added Weather API to access weather data

---
+++ weather-app/app.js
@@ -0,0 +1,65 @@
+const request = require("request");
+
+const yargs = require("yargs");
+
+yargs.command({
+  command: "location",
+  describe: "Enter location",
+  builder: {
+    city: {
+      describe: "city name",
+      demandOption: true,
+      type: "string",
+    },
+    state: {
+      describe: "state name",
+      //demandOption: true,
+      //type: "string",
+    },
+    country: {
+      describe: "country name",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    const url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      argv.city +
+      "," +
+      argv.state +
+      "," +
+      argv.country +
+      "&APPID=62b4ca8c3ab560e2742636ac825805d5&units=metric";
+    getWeather(url);
+  },
+});
+
+const getWeather = (url) => {
+  request({ url: url, json: true }, (error, response) => {
+    const data = response.body;
+    const country = data.sys.country,
+      state = data.name,
+      temperature = data.main.temp,
+      pressure = data.main.pressure,
+      humidity = data.main.humidity,
+      visibility = data.visibility;
+    //console.log(data.main);
+    console.log(
+      "Country: " +
+        country +
+        " State: " +
+        state +
+        " Temperature: " +
+        temperature +
+        " Pressure: " +
+        pressure +
+        " Humidity: " +
+        humidity +
+        " Visibility: " +
+        visibility
+    );
+  });
+};
+
+yargs.parse();
