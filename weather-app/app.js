622e48c97166203a9553110797de4ff956c2568b
DeepakGitHub050 <ds1008076@gmail.com>
2022-06-30 15:24:21 +0530

    added location command to get weather of given location

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,6 +2,39 @@ const request = require("request");
 
 const yargs = require("yargs");
 
+yargs.command({
+  command: "location",
+  describe: "Enter location",
+  builder: {
+    city: {
+      describe: "city name",
+      demandOption: true,
+      type: "string",
+    },
+    state: {
+      describe: "state name",
+      //demandOption: true,
+      //type: "string",
+    },
+    country: {
+      describe: "country name",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    const url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      argv.city +
+      "," +
+      argv.state +
+      "," +
+      argv.country +
+      "&APPID=62b4ca8c3ab560e2742636ac825805d5&units=metric";
+    getWeather(url);
+  },
+});
+
 const getWeather = (url) => {
   request({ url: url, json: true }, (error, response) => {
     const data = response.body;
